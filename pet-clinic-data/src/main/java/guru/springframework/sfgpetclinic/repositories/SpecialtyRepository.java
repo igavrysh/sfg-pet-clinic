072967ba32306406291c74347732c9cae94e90c1
Ievgen Gavrysh <zhenya.ga@gmail.com>
2019-07-28 17:45:29 -0700

    Added Specialty SD JPA Service. Closes #47

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialtyRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.sfgpetclinic.repositories;
-
-import guru.springframework.sfgpetclinic.model.PetType;
-import guru.springframework.sfgpetclinic.services.CrudService;
-
-public interface SpecialtyRepository extends CrudService<PetType, Long> {
-}
